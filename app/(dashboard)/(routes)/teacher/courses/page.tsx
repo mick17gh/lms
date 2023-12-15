9a8fd79caf2a09734da0015444a359158448e6c0
mick17gh <mike17gh@gmail.com>
2023-12-15 10:02:59 +0000

    fixes

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const Courses = () => {
     return ( 
-        <div>
-            Courses
+        <div className="p-6">
+            <Link href="/teacher/create">
+                <Button>
+                    New Course
+                </Button>
+            </Link>
+            
         </div>
      );
 }
